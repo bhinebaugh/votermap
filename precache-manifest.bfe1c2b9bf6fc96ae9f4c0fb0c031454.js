f19e7b94bc420e3f1881a026158d1df7e8f28908
Byron Hinebaugh <bhinebaugh@gmail.com>
2019-06-20 14:49:35 -0400

    publish

---
+++ precache-manifest.bfe1c2b9bf6fc96ae9f4c0fb0c031454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4c03c6749afa68cacf1f50eeb3ddcf",
+    "url": "/voterviz/index.html"
+  },
+  {
+    "revision": "7c62fbf431444da6f2f3",
+    "url": "/voterviz/static/css/main.53fc0e4e.chunk.css"
+  },
+  {
+    "revision": "826948fddc632c62d39d",
+    "url": "/voterviz/static/js/2.db7d9b95.chunk.js"
+  },
+  {
+    "revision": "7c62fbf431444da6f2f3",
+    "url": "/voterviz/static/js/main.3257ec96.chunk.js"
+  },
+  {
+    "revision": "3512a71e1028747bf0ac",
+    "url": "/voterviz/static/js/runtime~main.bb0181c8.js"
+  }
+]);
